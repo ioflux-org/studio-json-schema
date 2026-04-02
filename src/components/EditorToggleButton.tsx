a748ce7b7fdbd3b1141028bc57d459560f4f79a0
Chanrattnak Mong <mongchanrattnak@gmail.com>
2026-04-02 12:30:23 -0400

    fix: stack chevrons vertically and swap padding orientation on mobile

--- src/components/EditorToggleButton.tsx
+++ src/components/EditorToggleButton.tsx
@@ -1,33 +1,49 @@
-import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
+import { BsChevronLeft, BsChevronRight, BsChevronUp, BsChevronDown } from "react-icons/bs";
 import { Tooltip } from "react-tooltip";
 
 const EditorToggleButton = ({
   className,
   editorVisible,
   toggleEditorVisibility,
+  isMobile = false,
 }: {
   className: string;
   editorVisible: boolean;
   toggleEditorVisibility: () => void;
+  isMobile?: boolean;
 }) => {
   return (
     <div className={className}>
       <button
         onClick={toggleEditorVisibility}
-        className="flex px-1 py-2 rounded-lg cursor-pointer bg-[var(--view-bg-color)] duration-300 border-2 hover:scale-105 text-[var(--navigation-text-color)]"
+        className={`flex ${isMobile ? "px-2 py-1" : "px-1 py-2"} rounded-lg cursor-pointer bg-[var(--view-bg-color)] duration-300 border-2 hover:scale-105 text-[var(--navigation-text-color)]`}
         data-tooltip-id="editor-toggle-tooltip"
         aria-label={editorVisible ? "Hide Editor" : "Show Editor"}
       >
-        {editorVisible ? (
-          <>
-            <BsChevronLeft size={10} />
-            <BsChevronLeft size={10} />
-          </>
+        {isMobile ? (
+          editorVisible ? (
+            <span className="flex flex-col">
+              <BsChevronDown size={10} />
+              <BsChevronDown size={10} />
+            </span>
+          ) : (
+            <span className="flex flex-col-reverse">
+              <BsChevronUp size={10} />
+              <BsChevronUp size={10} />
+            </span>
+          )
         ) : (
-          <>
-            <BsChevronRight size={10} />
-            <BsChevronRight size={10} />
-          </>
+          editorVisible ? (
+            <>
+              <BsChevronLeft size={10} />
+              <BsChevronLeft size={10} />
+            </>
+          ) : (
+            <>
+              <BsChevronRight size={10} />
+              <BsChevronRight size={10} />
+            </>
+          )
         )}
       </button>
       <Tooltip
